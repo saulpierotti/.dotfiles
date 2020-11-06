c949d6335ffb94ffc48c732036999c1f46bede39
saulpierotti <saulpierotti@gmail.com>
2020-11-06 19:05:54 +0300

    .

--- home/saul/.config/polybar/scripts/usb_detect.sh
+++ home/saul/.config/polybar/scripts/usb_detect.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+symbol=""
 usb_devs=`lsblk -l -o PATH,LABEL|grep -v '/dev/sda'|grep -v '/dev/sd. '|grep /dev|tr '\n' ' '|xargs`
 
-if [ -n "`lsblk -l -o PATH,LABEL|grep -v '/dev/sda'|grep -v '/dev/sd. '|grep /dev`" ]; then
-    echo $usb_devs
-else
-    echo ""
+if [[ ! -z "$usb_devs" ]]; then
+    spacer="  "
 fi
+
+# echo *"$symbol$spacer$usb_devs"*
+echo "$symbol$spacer$usb_devs"
